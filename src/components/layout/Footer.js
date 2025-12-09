03b2b67d4d8bcd91057a9d906300bf68f1e7e23b
AdissonPires <adissonfilho@gmail.com>
2025-12-09 11:17:40 -0300

    Adicionando favicon e ajustes finais

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,9 +4,9 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <ul className={styles.social_list}>
-        <li><a href="https://www.facebook.com/facebookBrasil/">Facebook</a></li>
-        <li><a href="https://www.instagram.com/">Instagram</a></li>
-        <li><a href="https://www.linkedin.com/in/adisson-pires-b23bb9354/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BWpyuTEPJTh6a4hj%2FPegS%2FA%3D%3D">LinkedIn</a></li>
+        <li><a href="https://www.facebook.com/facebookBrasil/"target="_blank">Facebook</a></li>
+        <li><a href="https://www.instagram.com/"target="_blank">Instagram</a></li>
+        <li><a href="https://www.linkedin.com/in/adisson-pires-b23bb9354/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BWpyuTEPJTh6a4hj%2FPegS%2FA%3D%3D" target="_blank">LinkedIn</a></li>
       </ul>
       <p>SaúdePA &copy; 2025 - Todos os direitos reservados.</p>
       <p>Adisson Filho - Lab. De Desenvolvimento Web</p>
